c85ed310bfa9826c293aed48aa742b0e7364c3db
Erick Simões <erick.simoes@live.com>
2018-05-27 02:43:31 -0300

    Implementação de funções para cada letra
    Cada letra tem uma função a(), b(), c()...

--- src/MorseSimples.cpp
+++ src/MorseSimples.cpp
@@ -14,6 +14,7 @@
 #define DELAY_PONTO 75
 #define DELAY_TRACO 375
 #define DELAY_INTERNO 125
+#define DELAY_CARACTERE 250
 
 MorseSimples::MorseSimples(int pino) {
 	_pino = pino;
@@ -32,4 +33,190 @@ void MorseSimples::traco() {
 	delay(DELAY_TRACO);
 	digitalWrite(_pino, LOW);
 	delay(DELAY_INTERNO);
+}
+
+void MorseSimples::a() {
+	ponto();
+	traco();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::b() {
+	traco();
+	ponto();
+	ponto();
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::c() {
+	traco();
+	ponto();
+	traco();
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::d() {
+	traco();
+	ponto();
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::e() {
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::f() {
+	ponto();
+	ponto();
+	traco();
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::g() {
+	traco();
+	traco();
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::h() {
+	ponto();
+	ponto();
+	ponto();
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::i() {
+	ponto();
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::j() {
+	ponto();
+	traco();
+	traco();
+	traco();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::k() {
+	traco();
+	ponto();
+	traco();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::l() {
+	ponto();
+	traco();
+	ponto();
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::m() {
+	traco();
+	traco();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::n() {
+	traco();
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::o() {
+	traco();
+	traco();
+	traco();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::p() {
+	ponto();
+	traco();
+	traco();
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::q() {
+	traco();
+	traco();
+	ponto();
+	traco();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::r() {
+	ponto();
+	traco();
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::s() {
+	ponto();
+	ponto();
+	ponto();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::t() {
+	traco();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::u() {
+	ponto();
+	ponto();
+	traco();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::v() {
+	ponto();
+	ponto();
+	ponto();
+	traco();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::w() {
+	ponto();
+	traco();
+	traco();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::x() {
+	traco();
+	ponto();
+	ponto();
+	traco();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::y() {
+	traco();
+	ponto();
+	traco();
+	traco();
+	delay(DELAY_CARACTERE);
+}
+
+void MorseSimples::z() {
+	traco();
+	traco();
+	ponto();
+	ponto();
+	delay(DELAY_CARACTERE);
 }
--- src/MorseSimples.h
+++ src/MorseSimples.h
@@ -23,6 +23,33 @@ class MorseSimples {
 			MorseSimples(int pino);
 			void ponto();
 			void traco();
+
+			void a();
+			void b();
+			void c();
+			void d();
+			void e();
+			void f();
+			void g();
+			void h();
+			void i();
+			void j();
+			void k();
+			void l();
+			void m();
+			void n();
+			void o();
+			void p();
+			void q();
+			void r();
+			void s();
+			void t();
+			void u();
+			void v();
+			void w();
+			void x();
+			void y();
+			void z();
 	private:
 			int _pino;
 };
